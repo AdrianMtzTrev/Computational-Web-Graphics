fdb0fea663a75a09e84c5704f3b936286657a4b0
Adrian Martinez Treviño <adrianmtztrev@hotmail.com>
2026-06-06 13:45:11 -0600

    fix: preserve player meshes across room switches

--- js/scene-manager.js
+++ js/scene-manager.js
@@ -15,8 +15,21 @@ export class SceneManager {
       if (current && current.unload) current.unload();
     }
 
+    const playerMeshes = [];
+    for (let i = this.scene.children.length - 1; i >= 0; i--) {
+      const child = this.scene.children[i];
+      if (child.userData?.isPlayer) {
+        this.scene.remove(child);
+        playerMeshes.push(child);
+      }
+    }
+
     while (this.scene.children.length > 0) {
       this.scene.remove(this.scene.children[0]);
+    }
+
+    for (const m of playerMeshes) {
+      this.scene.add(m);
     }
 
     const room = this.rooms.get(id);
